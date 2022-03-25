39ae0b74205271c6753c13fc6c0398ffbb71b8cc
Rakesh Gain <rakeshgain47681@gmail.com>
2022-03-25 08:56:25 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running at port 3000");
+})
