9b49ff39f14295682b149515e82bf90e5b6beaa1
Rakesh Gain <rakeshgain47681@gmail.com>
2022-03-29 10:14:20 +0530

    file updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const https = require("https")
 
 const app = express()
 
@@ -11,13 +12,62 @@ app.get("/", function(req, res){
 })
 
 app.post("/", function(req, res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    const data = {
+        members: [
+            {
+                email_address : email,
+                status : "subscribed",
+                merge_fields: {
+                    FNAME : firstName,
+                    LNAME : lastName,
+                }
+            }    
+        ]
+    };
 
-    console.log(firstName+" "+lastName+"   "+email)
+    const jsonData = JSON.stringify(data);
+    const url = "https://us14.api.mailchimp.com/3.0/lists/f1e4acc073"
+    const options = {
+        method: "POST",
+        auth: "Juli:27daa52fcee48e999a13b1afd643fcb0-us14",
+    }
+
+    const request = https.request(url, options, function(response){
+
+
+        if (response.statusCode === 200 )
+        {
+            // res.send(" You are success fully signuped !")
+            res.sendFile(__dirname+"/success.html")
+        }
+        else{
+            res.sendFile(__dirname +"/failure.html")
+        }
+
+        response.on(data,function(data){
+            console.log(JSON.parse(data));
+        });
+
+    })
+    request.write(jsonData);
+    request.end();
+    // console.log(firstName+" "+lastName+"   "+email)
 })
 
-app.listen(3000, function(){
+// Failure routs
+app.post("/failure", function(req ,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is running at port 3000");
 })
+
+
+// API Key
+// 27daa52fcee48e999a13b1afd643fcb0-us14
+// audince Id
+// f1e4acc073
